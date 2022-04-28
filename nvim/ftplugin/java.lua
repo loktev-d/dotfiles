ed9d2fe826dc036a39deb3d63804145cfadd1230
loktev-d <lokt.daniil@gmail.com>
2022-04-28 14:39:28 +0300

    Add Lombok support

--- nvim/ftplugin/java.lua
+++ nvim/ftplugin/java.lua
@@ -10,9 +10,11 @@ local config = {
         '--add-modules=ALL-SYSTEM',
         '--add-opens', 'java.base/java.util=ALL-UNNAMED',
         '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+        '-javaagent:/usr/share/java/lombok/lombok.jar',
+        '-Xbootclasspath/a:/usr/share/java/lombok/lombok.jar',
         '-jar', vim.fn.expand('~/.local/share/jdtls/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar'),
         '-configuration', vim.fn.expand('~/.local/share/jdtls/config_linux'),
-        '-data', vim.fn.expand('~/.cache/jdtls')
+        '-data', vim.fn.expand('~/.cache/jdtls'),
     },
     on_attach = require('buf_keymaps'),
     root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew', 'pom.xml' }),
